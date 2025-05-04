29cc9f179269335d520ca671aadf084046e2c6bc
LitFill <marrazzy54@gmail.com>
2025-05-05 01:32:11 +0700

    feat(Main): use optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -3,62 +3,85 @@ module Main where
 import AST
 import Lexer
 import Parser
-import System.Environment (getArgs)
+import Options.Applicative qualified as O
 
-usage :: String
-usage = unlines
-    [ "Usage: riml [file|cmds]"
-    , ""
-    , "Commands:"
-    , "    <file>         transpile the .riml file into .html file"
-    , "    ast <file>     print AST"
-    , "    render <file>  print the rendered HTML"
-    , "    help           print this help"
-    , "    version        print the version"
-    ]
+data ProgOpt
+    = CmdAst FilePath
+    | CmdRender FilePath
+    | CmdTranspile { outputfile :: FilePath, inputfile :: FilePath }
+    deriving (Show)
+
+filepathArg :: String -> O.Parser FilePath
+filepathArg help = O.argument O.str
+    (  O.metavar "FilePath"
+    <> O.help help)
+
+astcmdP :: O.Parser ProgOpt
+astcmdP = CmdAst <$> filepathArg "input file to generate the AST from"
+
+rendercmdP :: O.Parser ProgOpt
+rendercmdP = CmdRender <$> filepathArg "input file to render the html"
+
+transpilecmdP :: O.Parser ProgOpt
+transpilecmdP = CmdTranspile
+    <$> O.option O.str
+        (  O.long "output"
+        <> O.short 'o'
+        <> O.metavar "FilePath"
+        <> O.help "output file path (optional)"
+        <> O.showDefault
+        <> O.value "index.html")
+    <*> filepathArg "input file path (required)"
+
+subcmdP :: O.Parser ProgOpt
+subcmdP = O.subparser (astcmd <> rendercmd <> transpilecmd)
+  where
+    mkcmd name cmd desc =
+        O.command name (O.info cmd (O.progDesc desc))
+
+    astcmd       = mkcmd "ast"       astcmdP       "Print the AST generated from input file."
+    rendercmd    = mkcmd "render"    rendercmdP    "Render the html generated from input file."
+    transpilecmd = mkcmd "transpile" transpilecmdP "Transpile input file to html."
 
 version :: String
 version = "riml v1.0.0.4"
 
--- NOTE: Do I need a library for this, like optparse-applicative?
-handleArgs :: [String] -> IO ()
-handleArgs = \case
-    ["version"]       -> putStrLn      version
-    [fname]           -> translateFile fname
-    ["ast",    fname] -> printAst      fname
-    ["render", fname] -> renderFile    fname
-    _                 -> putStrLn      usage
+versionflagP :: O.Parser (a -> a)
+versionflagP = O.infoOption version
+    (  O.long "version"
+    <> O.short 'v'
+    <> O.help "Show the version of this compiler."
+    )
+
+progoptsP :: O.Parser ProgOpt
+progoptsP = subcmdP O.<**> (versionflagP <*> O.helper)
+
+progInfo :: O.ParserInfo ProgOpt
+progInfo = O.info progoptsP
+    (  O.fullDesc
+    <> O.progDesc "This is the compiler for riml. Implemented in Haskell."
+    <> O.header   "the riml compiler"
+    )
+
+toAst :: String -> HtmlNode
+toAst = parse . alexScanTokens
 
 printAst :: FilePath -> IO ()
-printAst fname = do
-    input <- readFile fname
-    let tokens = alexScanTokens input
-    let ast = parse tokens
-    print ast
+printAst fname =
+    print =<< toAst <$> readFile fname
 
 renderFile :: FilePath -> IO ()
-renderFile fname = do
-    input <- readFile fname
-    let tokens = alexScanTokens input
-    let ast = parse tokens
-    putStrLn $ prettyRender ast
+renderFile fname =
+    putStrLn =<< prettyRender . toAst <$> readFile fname
 
-translateFile :: FilePath -> IO ()
-translateFile fname = do
-    input <- readFile fname
-    let tokens   = alexScanTokens input
-    let ast      = parse tokens
-    let rendered = prettyRender ast
-    let outname  = renameFile fname
-    writeFile outname "<!DOCTYPE html>\n"
-    appendFile outname rendered
-
-renameFile :: String -> String
-renameFile =
-    (++ "html")
-    . reverse
-    . dropWhile (/= '.')
-    . reverse
+translateFile :: FilePath -> FilePath -> IO ()
+translateFile fin fout =
+    writeFile fout =<<
+        ("<!DOCTYPE html>\n" ++) . prettyRender . toAst <$> readFile fin
 
 main :: IO ()
-main = getArgs >>= handleArgs
+main = O.execParser progInfo >>= \case
+    CmdAst       fname    -> printAst fname
+    CmdRender    fname    -> renderFile fname
+    CmdTranspile fout fin -> translateFile fin fout
+
